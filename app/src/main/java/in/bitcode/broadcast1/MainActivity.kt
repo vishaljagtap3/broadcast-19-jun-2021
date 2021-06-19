4f3b1bb8326044c02f29a38a735e3a576643e47d
vishaljagtap3 <vishal.jagtap@bitcode.in>
2021-06-19 15:03:53 +0530

    BroadcastReceiver

---
+++ app/src/main/java/in/bitcode/broadcast1/MainActivity.kt
@@ -0,0 +1,42 @@
+package `in`.bitcode.broadcast1
+
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btnRegister : Button
+    lateinit var btnUnRegister : Button
+    lateinit var br : BroadcastReceiver
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init()
+
+        btnRegister.setOnClickListener {
+            br = MyBroadcastReceiver()
+
+            var intentFilter = IntentFilter(Intent.ACTION_WALLPAPER_CHANGED)
+            intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+            intentFilter.addAction(Intent.ACTION_BATTERY_LOW)
+            intentFilter.addAction(Intent.ACTION_POWER_CONNECTED)
+
+            registerReceiver(br, intentFilter)
+        }
+
+        btnUnRegister.setOnClickListener {
+            unregisterReceiver(br)
+        }
+    }
+
+    private fun init() {
+        btnRegister = findViewById(R.id.btnRegister)
+        btnUnRegister = findViewById(R.id.btnUnRegister)
+    }
+}
---
+++ app/src/main/java/in/bitcode/broadcast1/MyBroadcastReceiver.kt
@@ -0,0 +1,23 @@
+package `in`.bitcode.broadcast1
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+
+class MyBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context, intent?.action, Toast.LENGTH_LONG).show()
+        Log.e("tag", intent?.action.toString())
+
+        if(intent?.action.equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)) {
+            if(intent?.getBooleanExtra("state", false) == true) {
+                Log.e("tag", "Airlplane mode - ON")
+            }
+            else {
+                Log.e("tag", "Airlplane mode - FF")
+            }
+        }
+    }
+}
